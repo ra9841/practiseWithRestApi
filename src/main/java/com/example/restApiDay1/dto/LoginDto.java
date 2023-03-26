82217f73256484d3908d054de869ce389893de7a
Rabin <rabin.shrestha7@gmail.com>
2023-03-26 14:38:49 -0700

    helloo

---
+++ src/main/java/com/example/restApiDay1/dto/LoginDto.java
@@ -0,0 +1,26 @@
+package com.example.restApiDay1.dto;
+
+public class LoginDto {
+	private String username;
+	private String password;
+	
+	public LoginDto() {
+		super();
+	//this is comment from github
+		//let me change something
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+	this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
